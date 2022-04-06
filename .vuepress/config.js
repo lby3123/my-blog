c113f6d0f701554c9423369ac583bcc4e28a2846
lby3123 <157620486@qq.com>
2022-04-06 21:57:45 +0800

    修复自动生成侧边栏的问题

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -36,7 +36,7 @@ module.exports = {
     search: true,
     searchMaxSuggestions: 10,
     // 自动形成侧边导航
-    sidebar: 'auto',
+    // sidebar: 'auto',
     // 最后更新时间
     lastUpdated: 'Last Updated',
     // 作者
@@ -62,7 +62,7 @@ module.exports = {
         },
       ],
     }],
-    // ["vuepress-plugin-auto-sidebar", {}]
+    ["vuepress-plugin-auto-sidebar", {}]
   ],
   markdown: {
     lineNumbers: true
